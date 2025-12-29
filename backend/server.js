8d4769077acabd32052217be00ab4ffff4499430
Devi-Velayudham <dev.peridot@gmail.com>
2025-12-30 01:44:07 +0530

    URL fix

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "http://localhost:5000",
+      "http://localhost:3000",
       "https://expense-tracker-ruby-mu.vercel.app",
     ],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
